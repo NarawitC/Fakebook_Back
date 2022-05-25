fcd44047916a9f953f7d11b44a7480f01e7439bd
NarawitC <narawit.developer@outlook.com>
2022-05-25 13:27:45 +0700

    Controller post-likePost, unLikePost

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const { Post } = require('../models/index');
+const { Post, Like, sequelize } = require('../models/index');
 const cloudinary = require('../utils/cloundinary');
 const createError = require('../utils/createError');
 
@@ -25,5 +25,57 @@ exports.createPost = async (req, res, next) => {
     if (file) {
       fs.unlinkSync(file.path);
     }
+  }
+};
+
+exports.likePost = async (req, res, next) => {
+  const transaction = await sequelize.transaction(); //* start transaction
+  try {
+    const { postId } = req.params;
+    const existLike = await Like.findOne({
+      where: { postId, userId: req.user.id },
+    });
+    if (existLike) {
+      createError('You already liked this post', 400);
+    }
+    const post = await Post.findOne({ where: { id: postId } });
+    if (!post) {
+      createError('Post not found', 400);
+    }
+    const like = await Like.create(
+      { postId, userId: req.user.id },
+      { transaction }
+    );
+
+    await post.increment('like', { by: 1, transaction });
+    await transaction.commit();
+    res.json({ like });
+  } catch (err) {
+    await transaction.rollback();
+    next(err);
+  }
+};
+
+exports.unLikePost = async (req, res, next) => {
+  const transaction = await sequelize.transaction(); //* start transaction
+  try {
+    const { postId } = req.params;
+    const liked = await Like.findOne({
+      where: { postId, userId: req.user.id },
+    });
+    if (!liked) {
+      createError('You never like this post', 400);
+    }
+    const post = await Post.findOne({ where: { id: postId } });
+    if (!post) {
+      createError('Post not found', 400);
+    }
+    await liked.destroy({ transaction });
+    await post.decrement('like', { by: 1, transaction });
+    await transaction.commit();
+    res.status(204).json();
+  } catch (err) {
+    await transaction.rollback();
+    next(err);
   }
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,5 +4,7 @@ const postController = require('../controllers/postController');
 const upload = require('../middlewares/upload');
 
 router.post('/', upload.single('image'), postController.createPost);
+router.post('/:postId/like', postController.likePost);
+router.delete('/:postId/like', postController.unLikePost);
 
 module.exports = router;
